f967b1dbfd6665adb57c2ca09f792d79e2d23030
Umer Sagri <umersagri2006+GitHub@gmail.com>
2026-05-04 13:59:17 +0530

    Delete backend directory

--- backend/translator/settings.py
+++
@@ -1,8 +0,0 @@
-REST_FRAMEWORK = {
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '10/minute',  # max 10 requests per user/IP per minute
-    }
-}
